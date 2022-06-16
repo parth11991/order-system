87390d895313bc50567a28fed02f71ddd760196d
parth-onfuro <parth.onfuro@gmail.com>
2022-06-16 16:15:03 +0100

    no message

---
+++ database/migrations/2022_06_16_121130_add_supplier_has_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSupplierHasItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('supplier_has_items', function (Blueprint $table) {
+            $table->string('supplier_code')->nullable();
+            $table->string('supplier_barcode')->nullable();
+            $table->time('lead_time')->nullable();
+            $table->decimal('supplier_price', 9, 2)->default(0)->nullable();
+            $table->string('supplier_currency')->default('GBP');
+            $table->integer('min_order_quantity')->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('supplier_has_items', function (Blueprint $table) {
+            $table->dropColumn('supplier_code');
+            $table->dropColumn('supplier_barcode');
+            $table->dropColumn('lead_time');
+            $table->dropColumn('supplier_price');
+            $table->dropColumn('supplier_currency');
+            $table->dropColumn('min_order_quantity');
+        });
+    }
+}
